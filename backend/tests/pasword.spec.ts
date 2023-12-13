34235de4ead1f22e239441be95d7225b74e8fcac
Thibault <97745143+thibaultdefour@users.noreply.github.com>
2023-12-13 13:49:34 +0000

    Backend CI : Unit Tests -> Use case

---
+++ backend/tests/pasword.spec.ts
@@ -0,0 +1,19 @@
+import { isValidPassword } from '../routes/auth'
+
+describe('Password strength test', () => {
+    it('Positive', () => {
+        expect(isValidPassword('Hello wor1d')).toBe(true)
+        expect(isValidPassword('H3llo wOrld')).toBe(true)
+        expect(isValidPassword('Hello wOrld!')).toBe(true)
+        expect(isValidPassword('He11o world')).toBe(true)
+        expect(isValidPassword('Hellow0rd')).toBe(true)
+    })
+
+    it('Negative', () => {
+        expect(isValidPassword('hello world')).toBe(false)
+        expect(isValidPassword('43110')).toBe(false)
+        expect(isValidPassword('hello')).toBe(false)
+        expect(isValidPassword('hell0')).toBe(false)
+        expect(isValidPassword('hello world!')).toBe(false)
+    })
+})
